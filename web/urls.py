0b9d284a668b46d00664a643ea593e55f54fd379
jackbicknell <jackbicknell@live.co.uk>
2020-01-27 21:43:24 +0000

    cert endpoint

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
-    HomeView)
+    HomeView, cert)
 from . import views
 
 urlpatterns = [
-    path('', HomeView, name="jm-home")
+    path('', HomeView, name="jm-home"),
+    path('.well-known/acme-challenge/7NoNFFaCwnNn0GvinW4OmK4hTBUG04Fqq3BAmkkI5Ds',
+         cert, name="jm-cert")
 ]
--- web/views.py
+++ web/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 def HomeView(request):
     return render(request, 'web/home.html', {'title': 'Jacuzzi Mansions'})
+
+def cert(request):
+    return HttpResponse('7NoNFFaCwnNn0GvinW4OmK4hTBUG04Fqq3BAmkkI5Ds.1gcphjPBdV1qYX2qcryv0wFejiQZpL_vaw3LgBwO9uk')
